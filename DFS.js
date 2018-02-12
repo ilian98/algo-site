ed38979285153a036bf6596f4f1d5cbcc63b6793
ILIAN\Ilian1 <ilian2105@abv.bg>
2018-02-12 18:47:08 +0200

    Made some code refactoring. Changed the animation of DFS. Fixed some issues in graph_drawer and removed the unnecessary svg file.

--- DFS.js
+++ DFS.js
@@ -1,61 +1,71 @@
-var s,vertices=[],edges=[];
-var adjList=[[1,2,3,4],[0,2],[0,1],[0],[0]],used=[],verticesCoord,animations=[];
+var s,verCircles=[],textCircles=[],edgeLines=[];
+var vertexRad=20,n=5,edgeList=[[1,2],[1,3],[1,4],[1,5],[2,3]],adjList=[[1,2,3,4],[0,2],[0,1],[0],[0]];
+var used=[],verCoord,animations=[];
 function loadSvg () {
-         s=Snap("#img");
-         var edgeList=[[1,2],[1,3],[1,4],[1,5],[2,3]],vertexR=20,n=5,edges=[];
-         verticesCoord=drawGraph(1,1,299,299,n,edgeList,vertexR);
+         s=Snap("#graph");
+         verCoord=drawGraph(1,1,299,299,n,edgeList,vertexRad);
          for (var i=0; i<edgeList.length; i++) {
-             var st=verticesCoord[edgeList[i][0]-1],end=verticesCoord[edgeList[i][1]-1];
-             edges[i]=s.line(st[0]+vertexR,st[1]+vertexR,end[0]+vertexR,end[1]+vertexR);
-             edges[i].attr({stroke: "black", "stroke-width": 1.5});
+             var st=verCoord[edgeList[i][0]-1],end=verCoord[edgeList[i][1]-1];
+             edgeLines[i]=s.line(st[0]+vertexRad,st[1]+vertexRad,end[0]+vertexRad,end[1]+vertexRad);
+             edgeLines[i].attr({stroke: "black", "stroke-width": 1.5});
              }
          for (var i=0; i<n; i++) {
-             vertices[i]=s.circle(verticesCoord[i][0]+vertexR,verticesCoord[i][1]+vertexR,vertexR,vertexR);
-             var text=s.text(verticesCoord[i][0]+vertexR,verticesCoord[i][1]+vertexR,(i+1).toString());
-             text.attr({"font-size": 25});
-             text.attr({x: text.getBBox().x-text.getBBox().w/2, y:text.getBBox().y+text.getBBox().h, class: "unselectable"});
-             vertices[i].attr({fill: "white", stroke: "black", "stroke-width": 1.5});
+             verCircles[i]=s.circle(verCoord[i][0]+vertexRad,verCoord[i][1]+vertexRad,vertexRad,vertexRad);
+             verCircles[i].attr({fill: "white", stroke: "black", "stroke-width": 1.5});
+             textCircles[i]=s.text(verCoord[i][0]+vertexRad,verCoord[i][1]+vertexRad,(i+1).toString());
+             textCircles[i].attr({"font-size": 25});
+             textCircles[i].attr({x: textCircles[i].getBBox().x-textCircles[i].getBBox().w/2, y:textCircles[i].getBBox().y+textCircles[i].getBBox().h, class: "unselectable"});
              }
 }
 function start () {
          used=[];
-         for (var i=0; i<5; i++) {
+         for (var i=0; i<n; i++) {
              used[i]=0;
              }
-         animations.push([1,[0,0,"red"]])
+         animations.push([[0,0,"red"]])
          dfs(0);
          var animFuncs=[];
          for (i=animations.length-1; i>=0; i--) {
              animFuncs[i]={
                 index: i,
                 func: function () {
-                         var i=this.index,cur;
-                         for (var j=1; j<animations[i][0]; j++) {
-                             cur=animations[i][j];
-                             if (cur[0]==0) vertices[cur[1]].animate({fill: cur[2]},1000);
+                         var i=this.index,curAnim,animLen=animations[i].length;
+                         for (var j=0; j<animLen-1; j++) {
+                             curAnim=animations[i][j];
+                             if (curAnim[0]==0) verCircles[curAnim[1]].animate({fill: curAnim[2]},1000);
                              else { var stx,sty,endx,endy;
-                                    stx=verticesCoord[cur[1]][0]+20; sty=verticesCoord[cur[1]][1]+20;
-                                    endx=verticesCoord[cur[2]][0]+20; endy=verticesCoord[cur[2]][1]+20;
-                                    var matrix=new Snap.matrix();
-                                    matrix.translate(endx-stx,endy-sty);
-                                    var cir=s.circle(stx,sty,2,2).attr({fill: "red"});
-                                    cir.animate({transform: matrix},1000,function () {
-                                        cir.remove();
-                                        });
+                                    stx=verCoord[curAnim[1]][0]+20; sty=verCoord[curAnim[1]][1]+20;
+                                    endx=verCoord[curAnim[2]][0]+20; endy=verCoord[curAnim[2]][1]+20;
+                                    var path="M "+stx.toString()+","+sty.toString()+" L"+endx.toString()+","+endy.toString();
+                                    var length=Snap.path.getTotalLength(path);
+                                    var lineDraw=s.path(path);
+                                    lineDraw.attr({fill: "none", stroke: "red", "stroke-width": 2.5,
+                                                   "stroke-dasharray": length.toString()+" "+length.toString(),
+                                                   "stroke-dashoffset": length, "stroke-linecap": "round",
+                                                   "stroke-linejoin": "round", "stroke-miterlimit": 10});
+                                    s.append(verCircles[curAnim[1]]); s.append(textCircles[curAnim[1]]);
+                                    s.append(verCircles[curAnim[2]]); s.append(textCircles[curAnim[2]]);
+                                    lineDraw.animate({strokeDashoffset: 0},1000);
                                     }
                              }
-                         cur=animations[i][animations[i][0]];
-                         if (cur[0]==0) vertices[cur[1]].animate({fill: cur[2]},1000,function () {
+                         curAnim=animations[i][animLen-1];
+                         if (curAnim[0]==0) verCircles[curAnim[1]].animate({fill: curAnim[2]},1000,function () {
                                             if (i!=animations.length-1) animFuncs[i+1].func();
                                             });
                          else { var stx,sty,endx,endy;
-                                stx=verticesCoord[cur[1]][0]+20; sty=verticesCoord[cur[1]][1]+20;
-                                endx=verticesCoord[cur[2]][0]+20; endy=verticesCoord[cur[2]][1]+20;
-                                var matrix=new Snap.matrix();
-                                matrix.translate(endx-stx,endy-sty);
-                                var cir=s.circle(stx,sty,2,2).attr({fill: "red"});
-                                cir.animate({transform: matrix},1000,function () {
-                                    cir.remove();
+                                stx=verCoord[curAnim[1]][0]+20; sty=verCoord[curAnim[1]][1]+20;
+                                endx=verCoord[curAnim[2]][0]+20; endy=verCoord[curAnim[2]][1]+20;
+                                var path="M "+stx.toString()+","+sty.toString()+" L"+endx.toString()+","+endy.toString();
+                                var length=Snap.path.getTotalLength(path);
+                                var lineDraw=s.path(path);
+                                lineDraw.attr({fill: "none", stroke: "red", "stroke-width": 2.5,
+                                               "stroke-dasharray": length.toString()+" "+length.toString(),
+                                               "stroke-dashoffset": length, "stroke-linecap": "round",
+                                               "stroke-linejoin": "round", "stroke-miterlimit": 10});
+                                s.append(verCircles[curAnim[1]]); s.append(textCircles[curAnim[1]]);
+                                s.append(verCircles[curAnim[2]]); s.append(textCircles[curAnim[2]]);
+                                lineDraw.animate({strokeDashoffset: 0},1000,function () {
+                                    lineDraw.remove();
                                     if (i!=animations.length-1) animFuncs[i+1].func();
                                     });
                                 }
@@ -69,11 +79,12 @@ function dfs (vr) {
          used[vr]=1;
          for (var i=0; i<adjList[vr].length; i++) {
              if (used[adjList[vr][i]]==0) {
-                if (fl!=0) animations.push([1,[0,vr,"red"]]);
-                animations.push([3,[0,vr,"white"],[1,vr,adjList[vr][i]],[0,adjList[vr][i],"red"]]);
+                if (fl!=0) animations.push([[0,vr,"red"]]);
+                animations.push([[0,vr,"white"],[1,vr,adjList[vr][i]]]);
+                animations.push([[0,adjList[vr][i],"red"]]);
                 dfs(adjList[vr][i]);
                 fl++;
                 }
              }
-        if (fl==0) animations.push([1,[0,vr,"white"]]);
+        if (fl==0) animations.push([[0,vr,"white"]]);
 }
--- graph_drawer.js
+++ graph_drawer.js
@@ -1,42 +1,47 @@
-function drawGraph (frameX, frameY, frameW, frameH, n, edgeList, vertexR) {
-         var adjMatrix=[],verCoord=[],pos=[],posLen=0;
-         for (i=0; i<edgeList.length; i++) {
+function circleSegment (segPoint1, segPoint2, center, rad) {
+         var area,height,sides=[];    
+         area=Math.abs(segPoint1[0]*segPoint2[1]+segPoint1[1]*center[0]+segPoint2[0]*center[1]-
+                       segPoint1[1]*segPoint2[0]-segPoint1[0]*center[1]-segPoint2[1]*center[0])/2;
+         sides[0]=Math.sqrt(Math.pow(segPoint1[0]-segPoint2[0],2)+Math.pow(segPoint1[1]-segPoint2[1],2));
+         sides[1]=Math.sqrt(Math.pow(segPoint1[0]-center[0],2)+Math.pow(segPoint1[1]-center[1],2));
+         sides[2]=Math.sqrt(Math.pow(segPoint2[0]-center[0],2)+Math.pow(segPoint2[1]-center[1],2));
+         if ((sides[0]*sides[0]+sides[2]*sides[2]-sides[1]*sides[1]>0)&&
+             (sides[0]*sides[0]+sides[1]*sides[1]-sides[2]*sides[2]>0)) {
+            height=area*2/sides[0];
+            if (height<=1.1*rad) return true;
+            }
+         return false;
+}
+function drawGraph (frameX, frameY, frameW, frameH) {
+         var adjMatrix=[],coord=[],possiblePos=[],possiblePosLen=0,dist;
+         for (var i=0; i<n; i++) {
              adjMatrix[i]=[];
+             for (var j=0; j<n; j++) {
+                 adjMatrix[i][j]=0;
+                 }
              }
          for (var i=0; i<edgeList.length; i++) {
-             adjMatrix[edgeList[i][0]][edgeList[i][1]]=1;   
+             adjMatrix[edgeList[i][0]-1][edgeList[i][1]-1]=adjMatrix[edgeList[i][1]-1][edgeList[i][0]-1]=1;
              }
-         for (var i=0; i<=(frameW-2*vertexR)/(3*vertexR); i++) {
-             for (var j=0; j<=(frameH-2*vertexR)/(3*vertexR); j++) {
-                 pos[posLen++]=[i*(3*vertexR)+frameX,j*(3*vertexR)+frameY];
+         dist=vertexRad/2+(Math.random()).toPrecision(4)*vertexRad/2;
+         for (var i=0; i<=(frameW-2*vertexRad)/(2*vertexRad+dist); i++) {
+             for (var j=0; j<=(frameH-2*vertexRad)/(2*vertexRad+dist); j++) {
+                 possiblePos[possiblePosLen++]=[i*(2*vertexRad+dist)+frameX,j*(2*vertexRad+dist)+frameY];
                  }
              }
          for (var i=0; i<n; i++) {
-             var ind,j,h,fl,points=[],minX,maxX,cur,area,height;
-             curPos=pos;
+             var ind,j,h,fl,curPoint;
+             curpossiblePos=possiblePos.slice();
              for (;;) {
-                 if (curPos.length==0) return drawGraph(frameX,frameY,frameW,frameH,n,edgeList,vertexR);
-                 ind=parseInt(Math.random()*(10*curPos.length))%curPos.length;
-                 cur=curPos[ind];
+                 if (curpossiblePos.length==0) return drawGraph(frameX,frameY,frameW,frameH,n,edgeList,vertexRad);
+                 ind=parseInt(Math.random()*(10*curpossiblePos.length))%curpossiblePos.length;
+                 curPoint=curpossiblePos[ind];
                  fl=0;
                  for (j=0; j<i; j++) {
-                     for (h=0; h<j; h++) {
-                         if ((adjMatrix[i][j]==0)&&(adjMatrix[i][h]==0)&&(adjMatrix[j][h]==0)) continue;
-                         points[0]=verCoord[j]; points[1]=verCoord[h]; points[2]=cur;
-                         area=Math.abs(points[0][0]*points[1][1]+points[0][1]*points[2][0]+points[1][0]*points[2][1]-
-                                       points[0][1]*points[1][0]-points[0][0]*points[2][1]-points[1][1]*points[2][0])/2;
-                         height=area*2/Math.sqrt(Math.pow(points[0][0]-points[1][0],2)+Math.pow(points[0][1]-points[1][1],2));
-                         if (height<=11/10*vertexR) {
-                            fl++;
-                            break;
-                            }
-                         height=area*2/Math.sqrt(Math.pow(points[0][0]-points[2][0],2)+Math.pow(points[0][1]-points[2][1],2));
-                         if (height<=11/10*vertexR) {
-                            fl++;
-                            break;
-                            }
-                         height=area*2/Math.sqrt(Math.pow(points[1][0]-points[2][0],2)+Math.pow(points[1][1]-points[2][1],2));
-                         if (height<=11/10*vertexR) {
+                     if (adjMatrix[i][j]==0) continue;
+                     for (h=0; h<i; h++) {
+                         if (h==j) continue;
+                         if (circleSegment(curPoint,coord[j],coord[h],vertexRad)==true) {
                             fl++;
                             break;
                             }
@@ -44,15 +49,24 @@ function drawGraph (frameX, frameY, frameW, frameH, n, edgeList, vertexR) {
                      if (fl!=0) break;
                      }
                  if (fl!=0) {
-                    curPos.splice(ind,1);
+                    curpossiblePos.splice(ind,1);
                     continue;
                     }
-                 verCoord[i]=cur;
-                 pos.splice(pos.findIndex(function (elem) {
-                                return (elem==cur);
-                                }),1);
+                 coord[i]=curPoint;
+                 possiblePos.splice(possiblePos.findIndex(function (elem) {
+                     return (elem==curPoint);
+                     }),1);
+                 
+                 for (j=0; j<i; j++) {
+                     if (adjMatrix[i][j]==0) continue;
+                     for (h=0; h<possiblePos.length; h++) {
+                         if (circleSegment(curPoint,coord[j],possiblePos[h],vertexRad)==true) {
+                            possiblePos.splice(h,1); h--;
+                            }
+                         }
+                     }
                  break;
                  }
              }
-         return verCoord;
+         return coord;
 }
