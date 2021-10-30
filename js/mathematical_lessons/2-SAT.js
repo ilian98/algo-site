c8361f2b741d1a185fee12e846eaab386df2f8a8
ilian98 <ilian2105@abv.bg>
2021-10-30 18:24:30 +0300

    Updated proof for algorithm for finding a solution of 2-SAT. Made some fixes in 2-SAT lesson.

--- js/mathematical_lessons/2-SAT.js
+++ js/mathematical_lessons/2-SAT.js
@@ -3,20 +3,14 @@
     let graphs=[];
     function initExample (part) {
         part--;
-        graphs[part] = new Graph();
-        if (part===1) {
-            graphs[1].init(".twoSATexample"+part+" .graphExample",5,true,false);
-            $(".twoSATexample1 .formula").val("(a||b)&&(a||!c)&&(!a||!b)");
-            $(".twoSATexample1 .make").on("click",makeImplicationGraph.bind(this,1));
-            
-            makeImplicationGraph(part);
-        }
-        else if (part==2) {
-            graphs[2].init(".twoSATexample"+part+" .graphExample",5,true,false,colourSCC.bind(this,graphs[2]));
-            $(".twoSATexample2 .formula").val("(a||b)&&(a||!c)&&(!a||!b)");
-            $(".twoSATexample2 .show").on("click",showSCC);
-            showSCC();
-        }
+        let showButton=$(".twoSATexample"+part+((part===1)?" .make":" .show"));
+        showButton.on("click",(part===1)?makeImplicationGraph.bind(this,part):showSCC.bind(this,part));
+        $(".twoSATexample"+part+" .default").on("click",function () {
+            graphs[part]=new Graph();
+            graphs[part].init(".twoSATexample"+part+" .graphExample",5,true);
+            $(".twoSATexample"+part+" .formula").val("(a||b)&&(a||!c)&&(!a||!b)"+((part===3)?"&&(a||!b)":""));
+            showButton.click();
+        }).click();
     }
 
     function findImplications (implications, formula) {
@@ -103,17 +97,19 @@
         let edgeList=[];
         graph.initVertices(2*implications.length);
         for (let i=0; i<implications.length; i++) {
-            if (variables.has(implications[i][0])===false) {
-                variables.set(implications[i][0],ind);
-                graph.vertices[ind].name=implications[i][0];
-                ind++;
+            for (let j=0; j<2; j++) {
+                let curr=implications[i][j];
+                if (variables.has(curr)===true) continue;
+                variables.set(curr,ind);
+                graph.vertices[ind++].name=curr;
+                
+                let negative;
+                if (curr[0]==='!') negative=curr.substr(1,curr.length-1);
+                else negative="!"+curr;
+                variables.set(negative,ind);
+                graph.vertices[ind++].name=negative;
             }
             x=variables.get(implications[i][0]);
-            if (variables.has(implications[i][1])===false) {
-                variables.set(implications[i][1],ind);
-                graph.vertices[ind].name=implications[i][1];
-                ind++;
-            }
             y=variables.get(implications[i][1]);
             edgeList.push([x,y]);
         }
@@ -130,53 +126,64 @@
         }
         order.push(vr);
     }
-    function dfs2 (vr, rev, used, num, comps) {
-        comps[num].push(vr);
+    let num,comps,nums;
+    function dfs2 (vr, rev, used) {
+        comps[num].push(vr); nums[vr]=num;
         used[vr]=true;
         for (let i=0; i<rev[vr].length; i++) {
             if (used[rev[vr][i]]===false) dfs2(rev[vr][i],rev,used,num,comps);
-            }
+        }
     }
 
-    let num,comps;
-    function colourSCC (graph) {
+    let values;
+    function colourSCC (graph, part) {
         let colours=["#f09481","#e66440","#de4026","#bd291e","#a6262f","#ba3a71","#e65c9a","#f777c2","#f094cd","#d97cc0",
                      "#c76dbf","#99498a","#80447f","#513d66","#3653b3","#248ad4","#5fcaed","#82ebf5","#17b2e6","#306ec9",
                      "#237040","#2d801b","#52992e","#66b324","#86cc14","#b0e627","#ffff69","#f7db02","#e8c00e","#f7aa25",
                      "#ff8c00","#ff8c00","#f0690e","#f0cab6","#e8bb97","#e09e75","#c9794b","#b06838","#ad6615","#733405",
                      "#542d01","#361c01","#574d43","#786e65","#b0a79d","#c7c5c3","#f2f2f2"];
         let jump=Math.floor(colours.length/num),colour=0;
+        let versColour=[];
         for (let i=0; i<num; i++) {
             for (let j=0; j<comps[i].length; j++) {
-                graph.svgVertices[comps[i][j]].circle.attr({fill: colours[colour]});
+                let v=comps[i][j];
+                if ((part===2)||(values.length===0)) {
+                    graph.vertices[v].addedCSS[0]="fill: "+colours[colour];
+                    versColour[v]=colours[colour];
+                }
+                else {
+                    if (values[v]===true) {
+                        graph.vertices[v].addedCSS[0]="fill: green";
+                        versColour[v]="green";
+                    }
+                    else {
+                        graph.vertices[comps[i][j]].addedCSS[0]="fill: red";
+                        versColour[v]="red";
+                    }
+                }
             }
             colour+=jump;
         }
         for (let i=0; i<graph.edgeList.length; i++) {
             if (graph.edgeList[i]===undefined) continue;
             let from=graph.edgeList[i].x,to=graph.edgeList[i].y;
-            if (graph.svgVertices[from].circle.attr("fill")==graph.svgVertices[to].circle.attr("fill")) {
-                graph.svgEdges[i].line.attr({stroke: graph.svgVertices[from].circle.attr("fill")});
-                graph.svgEdges[i].line.markerEnd.attr({fill: graph.svgVertices[from].circle.attr("fill")});
-            }
+            if (nums[from]===nums[to]) graph.edgeList[i].addedCSS[0]="stroke: "+versColour[from];
         }
+        graph.draw(false);
     }
-    function showSCC () {
-        makeImplicationGraph(2);
-        let graph=graphs[2],used=[];
+    function showSCC (part) {
+        makeImplicationGraph(part);
+        let graph=graphs[part],used=[];
         for (let i=0; i<graph.n; i++) {
-            if (graph.vertices[i]===undefined) continue;
             used[i]=false;
         }
         let order=[];
         for (let i=0; i<graph.n; i++) {
-            if (graph.vertices[i]===undefined) continue;
             if (used[i]===false) dfs1(i,graph.adjList,graph.edgeList,used,order);
         }
 
         let rev=[];
         for (let i=0; i<graph.n; i++) {
-            if (graph.vertices[i]===undefined) continue;
             used[i]=false;
             rev[i]=[];
         }
@@ -184,42 +191,41 @@
             if (edge===undefined) continue;
             rev[edge.y].push(edge.x);
         }
-        num=0; comps=[];
-        for (let i=graph.n-1; i>=0; i--) {
+        num=0; comps=[]; nums=[];
+        for (let i=order.length-1; i>=0; i--) {
             if (used[order[i]]===false) {
                comps[num]=[];
                dfs2(order[i],rev,used,num,comps);
                num++;
             }
         }
-
-        colourSCC(graph);
         
-        let solution=document.querySelector(".twoSATexample2 .solution"),text;
-        text="\\(";
+        let solution=document.querySelector(".twoSATexample"+part+" .solution"),text;
+        text="\\("; values=[];
         for (let i=0; i<graph.n; i++) {
-            if (graph.vertices[i]===undefined) continue;
             if (graph.vertices[i].name[0]==='!') continue;
-            let comp=[];
-            for (let j=0; j<num; j++) {
-                for (let h=0; h<comps[j].length; h++) {
-                    if (graph.vertices[comps[j][h]].name===graph.vertices[i].name) comp[0]=j;
-                    else if (graph.vertices[comps[j][h]].name==="!"+graph.vertices[i].name) comp[1]=j;
-                }
-            }
+            let comp=[nums[i], nums[i+1]];
             if (comp[0]===comp[1]) {
                 text="Няма решение, защото в силно-свързаната компонента на връх \\("+graph.vertices[i].name+"\\) има и неговото отрицание!";
+                values=[];
                 break;
             }
             if (text!="\\(") text+=", ";
             text+=graph.vertices[i].name;
-            if (comp[0]>comp[1]) text+=" = 1";
-            else text+=" = 0";
-            
-            if (i===graph.n-1) text+="\\)";
+            if (comp[0]>comp[1]) {
+                text+=" = 1";
+                values[i]=true; values[i+1]=false;
+            }
+            else {
+                text+=" = 0";
+                values[i]=false; values[i+1]=true;
+            }
         }
+        if (values.length!=0) text+="\\)";
         solution.textContent=text;
-        if (typeof MathJax!=="undefined") MathJax.typeset([".twoSATexample2 .solution"]);
+        if (typeof MathJax!=="undefined") MathJax.typeset([".twoSATexample"+part+" .solution"]);
+        
+        colourSCC(graph,part);
     }
     
     
