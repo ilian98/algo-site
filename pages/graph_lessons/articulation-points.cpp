f29683b567f380bc60eed4658eae2d3d65aed7f8
ilian98 <ilian2105@abv.bg>
2021-12-27 18:42:16 +0200

    Finished second part of articulation components lesson.

--- pages/graph_lessons/articulation-points.cpp
+++ pages/graph_lessons/articulation-points.cpp
@@ -1,4 +1,4 @@
-vector <int> a[MAXN]; /// списък на съседите
+vector <int> a[MAXN];
 bool used[MAXN];
 int in[MAXN],time;
 int up[MAXN];
@@ -6,15 +6,21 @@ void dfs (int vr, int fath) {
     used[vr]=true;
     in[vr]=time++;
     up[vr]=in[vr];
+    int flag=0;
     for (auto to : a[vr]) {
         if (used[to]==false) { /// дървесно ребро
             dfs(to,vr);
-            if (up[to]>=in[to]) cout << vr << " " << to << endl ; /// мост
-
             up[vr]=min(up[vr],up[to]);
+            if (up[to]>=in[vr]) flag++;
         }
         else if (to!=fath) { /// обратно ребро
             up[vr]=min(up[vr],in[to]);
         }
+    }
+    if (fath==-1) { /// vr е коренът
+        if (flag>1) cout << vr << endl ;
+    }
+    else {
+        if (flag>0) cout << vr << endl ;
     }
 }
